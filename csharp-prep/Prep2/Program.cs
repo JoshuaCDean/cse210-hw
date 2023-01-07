a1739b4bad5366e784bf139c73dda74519f72375
JoshuaCDean <u684188640@byui.edu>
2023-01-07 15:51:06 -0600

    Finished Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        string letter = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        if (number >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed with a {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry Unfortunately you failed. You got a {letter}. Study hard for next time!");
+        }
     }
 }
