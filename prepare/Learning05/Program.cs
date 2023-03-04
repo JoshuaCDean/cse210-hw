8f4fd887c81e5c2860858d7b0587605f38ecd885
JoshuaCDean <u684188640@byui.edu>
2023-03-04 15:47:36 -0600

    fin

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 10));
+        shapes.Add(new Rectangle("orange", 10,23));
+        shapes.Add(new Circle("purple", 19));
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"The Color of your shape is {color} and the area is {area}.");
+        }
     }
 }
